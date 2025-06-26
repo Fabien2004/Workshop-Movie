ae1bc01fe20980cf0d7c20d9872c41ead93a91f5
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-06-26 08:51:59 +0300

    add about

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,5 +5,8 @@ const router = Router();
 router.get('/', (req,res) =>{
     res.render('home');
 });
+router.get('/about', (req,res) =>{
+    res.render('home/about');
+});
 
 export default router;
