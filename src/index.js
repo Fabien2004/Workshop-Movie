615ed50cab9484dbed0eeedca50da56bd2223e2b
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-06-26 08:48:24 +0300

    add routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
   import express from 'express';
   import handlebars from 'express-handlebars';
-  import homeController from './controllers/homeController.js'
-
+  import routes from './routes.js'
+  
   const app = express();
 
   app.engine('hbs', handlebars.engine({
@@ -12,6 +12,6 @@
 
   app.use(express.static('public'));
  
-  app.use(homeController);
+  app.use(routes);
  
   app.listen(5000, () => console.log('Server is listening on http://localhost:5000'));
